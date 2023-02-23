f2f60bd5d6773c063dc674f015b95078eba08c65
Diana Pena <dianaligiapena@gmail.com>
2023-02-23 20:25:22 +0200

    country details

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,12 @@ import countriesJSON from './countries.json'
 
 import NavBar from './components/NavBar'
 import CountriesList from './components/CountriesList'
+import CountryDetails from './components/CountryDetails'
+
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
+
   const [countries, setCountries] = useState(countriesJSON)
 
 
@@ -14,7 +18,20 @@ function App() {
 
     <NavBar />
 
-    <CountriesList countries={countries} />
+    <div className='container'>
+      <div className='row'>
+
+        <CountriesList countries={countries} />
+
+        <Routes>
+            <Route 
+              path = "/:alpha3Code" 
+              element = { <CountryDetails countries = {countries} /> } 
+            />
+        </Routes>
+
+      </div>
+    </div>
 
     </div>
   )
